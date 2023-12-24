cc1f65c5fb8a002e1c991744e585edab770538b5
Pradyumnakg <79010579+Pradyumnakg@users.noreply.github.com>
2023-12-24 03:17:11 +0000

    basic

---
+++ basic/memory.js
@@ -0,0 +1,26 @@
+//**************************** */
+//Stack(Primitive),Heap(non primitive)
+
+
+
+let myName="Pradyumnn KUmar Mishra"
+// console.log(myName);
+let anothername=myName
+anothername="Mishra"
+console.log(myName);
+console.log(anothername);
+
+let userone={
+    Email: "pradyumnmaa@gmail.com",
+    Age: 44
+}
+
+console.log(userone);
+
+let usertwo=userone
+console.log(usertwo);
+
+usertwo.Email="bheemmaa@gmail.com"
+
+console.log(userone);
+console.log(usertwo);
---
+++ basic/string.js
@@ -0,0 +1,21 @@
+const name="pradyumn"
+
+console.log(name);
+const age =24
+console.log(name+""+age);
+
+console.log(name.length);
+
+const newname=new String("Pradyumn Kumar Mishtra")
+console.log(newname.toUpperCase());
+
+console.log(newname.charAt(5));
+
+console.log(newname.indexOf('M'));
+
+const newstring=newname.substring(2,6)
+const anothestring=newname.slice(-8,3)
+
+console.log(newstring);
+console.log(anothestring);
+
